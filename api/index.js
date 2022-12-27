dff056ff9bb3dfc62bdc24e7a941894d0e54620e
Umar Chowdhury <umarfchy@gmail.com>
2022-12-27 23:05:25 +0600

    add express api end points

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 "use strict";
-
 import express from "express";
+import cors from "cors";
+
+// configs
+const PORT =  5000;
 
 // dummy data
 const newsData = [
@@ -13,13 +16,22 @@ const newsData = [
 
 // base express app
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// dont forget to add cors later
-
 // '/' endpoint to test the server
+app.get("/", (_, res) => res.status(200).send("Hello World!"));
 
 // '/get' endpoint to get all news
+app.get("/get", (_, res) => res.status(200).send(newsData));
 
 // set '/create' endpoint to create a new news
+app.post("/create", (req, res) => {
+  const { text } = req.body;
+  const id = newsData.length + 1;
+  newsData.push({ id, text });
+  res.status(201).send({ message: "News created successfully" });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
