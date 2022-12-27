298948aa80e9153699f1b2826de6e21475b54f60
Umar Chowdhury <umarfchy@gmail.com>
2022-12-27 22:12:02 +0600

    add incomplete client version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
+// dummy data
+const newsList = [
+  { id: 1, text: "lorem ipsum dolor sit amet" },
+  { id: 2, text: "consectetur adipisicing elit" },
+  { id: 3, text: "laudantium esse eum ex qui" },
+  { id: 4, text: "fugiat facilis similique illum" },
+  { id: 5, text: "aliquid, tempora et excepturi" },
+];
+
 const App = () => {
+  // set state for news list
+
+  // set state for client input
+
+  // handle client input
+  // update id using array length
+
   return (
-    <main>
-      <p className="text-center my-5 text-lg">
-        Welcome to Poridhi's Workshop on
-      </p>
-      <p className="text-center font-bold my-5 text-lg">
-        Full Stack Engineering
-      </p>
-      <img src="/logo.png" alt="poridhi-logo" className="mx-auto w-96" />
+    <main className="grid place-items-center my-10">
+      <section>{/* section to handle input */}</section>
+      <section>{/* section to handle view */}</section>
     </main>
   );
 };
